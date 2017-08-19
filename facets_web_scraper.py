dc51d7bbca178022984711d79a7ff92a13b589b2
Dinesh Ladi <ladidinesh@gmail.com>
2017-08-19 15:06:20 +0530

    Update facets_web_scraper.py
    Added comments to the code

--- facets_web_scraper.py
+++ facets_web_scraper.py
@@ -1,7 +1,7 @@
 import os, sys, threading, subprocess, requests, urllib 
 from bs4 import BeautifulSoup
 
-# Create  directory 
+# Create a directory 
 os.makedirs("Facets365Wallpapers",exist_ok=True)
 main_url = "http://www.facets.la/wallpapers/"
 
@@ -12,12 +12,15 @@ print(res.text[0:100])
 soup = BeautifulSoup(res.text,'html.parser')
 
 links_list = []
+# Looping through all links
 for x in soup.find_all('a'):
 	url = x.get('href')
 	links_list.append(url)
 
+# Storing links only with wallpaper jpgs
 links_list = links_list[12:len(links_list)]
 
+# Looping through each link and get actual source of image and putting in the created folder
 for link in links_list:
 	mini_soup = BeautifulSoup(requests.get(link).text,'html.parser')
 	
@@ -26,6 +29,7 @@ for link in links_list:
 		if mini_link.startswith("http://www.facets.la/wallpaper/"):
 			myImage = requests.get(mini_link)
 			wallpaper_name = mini_link.split('/')[4]
+			print("Downloading... "+wallpaper_name)
 			imageFile = open(os.path.join("Facets365Wallpapers", wallpaper_name), 'wb')
 			for data in myImage.iter_content(100000):
 				imageFile.write(data)
